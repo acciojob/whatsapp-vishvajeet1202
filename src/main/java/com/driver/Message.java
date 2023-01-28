51d7df7fdb1a3ac02e05411f99e0967db595d52b
Vishvajeet <112777486+vishvajeet1202@users.noreply.github.com>
2023-01-28 20:15:08 +0530

    Update Message.java

--- src/main/java/com/driver/Message.java
+++ src/main/java/com/driver/Message.java
@@ -1,5 +1,4 @@
 package com.driver;
-
 import java.util.Date;
 
 public class Message {
@@ -7,4 +6,35 @@ public class Message {
     private String content;
     private Date timestamp;
 
+    public Message() {
+    }
+
+    public Message(int id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Date getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(Date timestamp) {
+        this.timestamp = timestamp;
+    }
 }
